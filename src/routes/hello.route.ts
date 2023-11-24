c525e1ac0769527c221cc372c14acadad0910f06
Nuwan Jaliyagoda <nuwan.jaliyagoda@rhino-partners.com>
2023-11-24 10:46:54 +0530

    Basic validation

--- src/routes/hello.route.ts
+++ src/routes/hello.route.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { helloWorld } from "../controllers/hello.controller";
+import { body, param, query, validationResult } from "express-validator";
 
 const router = Router();
 
@@ -17,4 +18,33 @@ router.get("/name/:name", (req, res) => {
     res.send({ msg: `Hello ${name}` });
 });
 
+router.post(
+    "/:version/register",
+    [
+        body("username")
+            .isLength({ min: 5 })
+            .withMessage("Username must be at least 5 characters long"),
+        body("email").isEmail().withMessage("Invalid email"),
+        body("password")
+            .isLength({ min: 5 })
+            .withMessage("Username must be at least 5 characters long"),
+        query("udid").optional().isBoolean(),
+        param("version").isIn(["v1", "v2"]),
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { username, email, password } = req.body;
+        const { udid } = req.query;
+        const { version } = req.param;
+
+        // Perform operations with validated data, e.g., save to database
+        console.log(username, password, email, udid, version);
+
+        res.status(200).json({ message: "User registered successfully" });
+    }
+);
 export default router;
